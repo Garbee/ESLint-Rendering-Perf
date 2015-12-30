6edf4b5eb858325ec65ab9c1884c98672a7c7ecb
Jonathan Garbee <jonathan@garbee.me>
2015-12-30 10:36:50 -0500

    Add rules against scroll metrics.

---
+++ tests/lib/rules/no-scroll-metrics.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var rule = require('../../../lib/rules/no-scroll-metrics'),
+  RuleTester =  require('eslint').RuleTester;
+
+var ruleTester = new RuleTester();
+
+var invalidStatements = [];
+
+var properties = [
+  'scrollWidth',
+  'scrollHeight',
+  'scrollLeft',
+  'scrollTop'
+];
+
+properties.forEach(function(item) {
+  var invalid = {};
+  invalid.code = 'var foo = elem.' + item;
+  invalid.errors = [];
+  invalid.errors.push({
+    message: 'Box metric properties are being called which are known to affect rendering performance. ' +
+    'Property called - ' + item,
+    type: 'MemberExpression'
+  });
+  invalidStatements.push(invalid);
+});
+
+ruleTester.run('no-scroll-metrics', rule, {
+  valid: [],
+  invalid: invalidStatements
+});
